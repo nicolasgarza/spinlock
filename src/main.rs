1cdd34e5b8fc6ac1e5a4a54ff8bf9270d226b24e
Nicolas Garza <nicolas.garza@gmail.com>
2024-08-07 18:54:53 -0500

    basic spinlock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::sync::atomic::{AtomicBool, Ordering::Acquire, Ordering::Release};
+
+pub struct SpinLock {
+    locked: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+        }
+    }
+
+    pub fn lock(&self) {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
