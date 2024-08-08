b7d5999d73a6ad171eba7a97b11e5bb0453bc312
Nicolas Garza <nicolas.garza@gmail.com>
2024-08-08 09:58:49 -0500

    safe interface for lock with guard

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,80 @@
+use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
 use std::sync::atomic::{AtomicBool, Ordering::Acquire, Ordering::Release};
+use std::thread;
 
-pub struct SpinLock {
-    locked: AtomicBool,
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
 }
 
-impl SpinLock {
-    pub const fn new() -> Self {
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        // safety: very existence of guard
+        // garuntees we've exclusively locked the lock
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        // safety: very existence of guard
+        // garuntees we've exclusively locked the lock
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
+    }
+}
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+/*
+ * UnsafeCell does not implement sync, making the type no longer sharable between threads.
+ * To fix, we need to promise the type is actually safe by impl'ing Sync for every type that
+ * is Send.
+ */
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
         Self {
             locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
         }
     }
 
-    pub fn lock(&self) {
+    pub fn lock(&self) -> Guard<T> {
         while self.locked.swap(true, Acquire) {
             std::hint::spin_loop();
         }
+        Guard { lock: self }
     }
 
+    // Safety: The &mut T from lock() must be gone
+    // (and no cheating by keeping reference to fields of that T around)
     pub fn unlock(&self) {
         self.locked.store(false, Release)
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
 }
